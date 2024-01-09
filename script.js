41d14c7305885e9253723bed35235b54bddf5519
gokulgovinthan <goodgovinthan@gmail.com>
2024-01-09 15:01:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.getElementById('submitButton').onclick=function(){
+	const FirstName=document.getElementById('FirstName').value;
+	const LastName=document.getElementById('LastName').value;
+	const PhoneNumber=document.getElementById('PhoneNumber').value;
+	const EmailId=document.getElementById('Email').value;
 
+alert(`First Name: ${FirstName}\nLast Name: ${LastName}\nPhone Number: ${PhoneNumber}\nEmail ID: ${EmailId}`);
+}
